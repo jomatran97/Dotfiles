487b8696f61730a9b78f0cfd9a1b637e63ed850a
jomatran97 <jomatran97@gmail.com>
2022-11-11 09:46:43 +0700

    udpate somethong

--- nvim/lua/joma/base.lua
+++ nvim/lua/joma/base.lua
@@ -31,9 +31,9 @@ vim.opt.wildignore:append { '*/node_modules/*' }
 
 vim.opt.swapfile = false
 
--- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
 
 -- Turn off paste mode when leaving insert
 vim.api.nvim_create_autocmd("InsertLeave", {
@@ -43,3 +43,6 @@ vim.api.nvim_create_autocmd("InsertLeave", {
 
 -- Add asterisks in block comments
 vim.opt.formatoptions:append { 'r' }
+
+
+vim.cmd [[set nofixendofline]]

957e0080ed1eddfe8c6c7193f1e5985a7b6dd83c
Joma <haitran@AP-0821-1040.local>
2022-11-15 17:45:52 +0700

    disable change single quote to doule quotge

--- nvim/lua/joma/base.lua
+++ nvim/lua/joma/base.lua
@@ -33,6 +33,7 @@ vim.opt.swapfile = false
 
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
+vim.cmd([[let g:black_skip_string_normalization = 1]])
 
 
 -- Turn off paste mode when leaving insert
